07d4a085e4fac7cc9f9b13eec759a5cae3db485c
cinicDiver <oscarframirezp@hotmail.com>
2023-08-30 15:14:26 -0500

    Imported bootstrap into index to handle styling across front.

--- tasks_client/src/index.js
+++ tasks_client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 import "./styling/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
