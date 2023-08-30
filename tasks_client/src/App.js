ad94890323e1627d4f6b281530048f4207bade3f
cinicDiver <oscarframirezp@hotmail.com>
2023-08-30 14:11:55 -0500

    Deleted boilerplate code by npm cra.

--- tasks_client/src/App.js
+++ tasks_client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styling/App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ViewTasks from "./views/ViewTasks";
+import CreateTasks from "./views/CreateTasks";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavigationBar></NavigationBar>
+      <Routes>
+        <Route path="/" element={<Navigate to="/todo_tasks" />}></Route>
+        <Route path="/todo_tasks" element={<ViewTasks />}></Route>
+        <Route path="/todo_tasks_create" element={<CreateTasks />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
