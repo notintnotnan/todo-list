659ea4a0626acfe76ba3833559346d618f36ef88
cinicDiver <oscarframirezp@hotmail.com>
2023-09-02 17:10:06 -0500

    Reorganized task list item into card like html.

--- tasks_client/src/components/TaskListItem.js
+++ tasks_client/src/components/TaskListItem.js
@@ -8,17 +8,25 @@ export default function TaskListItem(props) {
 
   return (
     <>
-      <div className="row">
-        <h3
-          onClick={() => {
-            navigate(`/todo_tasks/${props.taskId}`);
-          }}
-        >
-          {props.taskTitle}
-          <span className={`badge bg${doneMarker}`}>{doneText}</span>
-        </h3>
-        <div>
-          <p className="row">{props.taskDesc}</p>
+      <div className="container taskItemCard">
+        <div className="row taskItemHead">
+          <h3
+            onClick={() => {
+              navigate(`/todo_tasks/${props.taskId}`);
+            }}
+          >
+            <span>#{props.taskId} </span>
+            {props.taskTitle}
+          </h3>
+        </div>
+        <div className="row taskItemBody">
+          <p>{props.taskDesc}</p>
+        </div>
+        <div className="row taskItemFooter">
+          <div className="col-3">
+            <span className={`badge bg${doneMarker}`}>{doneText}</span>
+          </div>
+          <div className="col-9">{/*TODO: Add task labels.*/}</div>
         </div>
       </div>
     </>
