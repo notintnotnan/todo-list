994971f3241acde42bd241d5296d5c062c44ba04
cinicDiver <oscarframirezp@hotmail.com>
2023-09-02 17:02:22 -0500

    Re organized navegation bar layout.

--- tasks_client/src/components/NavigationBar.js
+++ tasks_client/src/components/NavigationBar.js
@@ -2,11 +2,16 @@ import { Link } from "react-router-dom";
 export default function NavigationBar(props) {
   return (
     <>
-      <div>
-        <Link to="/">
-          <h1>TODO Task list</h1>
+      <div className="row align-items-center customNavbar">
+        <Link to="/" className="col-6">
+          <h1>TODOs</h1>
         </Link>
-        <Link to="/todo_tasks_create">Create Task</Link>
+        <Link to="/todo_tasks" className="col-3">
+          View Tasks
+        </Link>
+        <Link to="/todo_tasks_create" className="col-3">
+          Create Task
+        </Link>
       </div>
     </>
   );
