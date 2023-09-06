b735c4454a7b2ed5b9def8e9363c931b0da78cc2
cinicDiver <oscarframirezp@hotmail.com>
2023-09-06 15:07:48 -0500

    Separated crud methods to be handled separately.

--- todo_tasks/views.py
+++ todo_tasks/views.py
@@ -1,7 +1,49 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .serializer import TaskSerializer
 from .models import Task
 
-class TaskView(viewsets.ModelViewSet):
-    serializer_class = TaskSerializer
-    queryset = Task.objects.all()
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        "viewList":"api/v1/list/",
+        "viewTask":"",
+        "createTask":"",
+        "updateTask":"",
+        "deleteTask":"",
+    }
+
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = TaskSerializer(Task.objects.all(),many=True).data
+    return Response(tasks)
+
+@api_view(['GET'])
+def taskView(request,task_id):
+    tasks = TaskSerializer(Task.objects.get(id=task_id),many=False).data
+    return Response(tasks)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def taskUpdate(request, task_id):
+    task = Task.objects.get(id=task_id)
+    serializer = TaskSerializer(instance=task,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+
+    return Response({"status":200})
